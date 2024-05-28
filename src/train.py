289d90a150c17429fdea2209ac45ab2a1a52a27e
VladislavZh <vlad18_97@mail.ru>
2024-05-28 20:40:46 +0300

    TopoHyperDrive

--- src/train.py
+++ src/train.py
@@ -108,7 +108,7 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
 
 
 @hydra.main(version_base="1.3", config_path="../configs", config_name="train.yaml")
-def main(cfg: DictConfig) -> Optional[float]:
+def main(cfg: DictConfig) -> Optional[float | tuple]:
     """Main entry point for training.
 
     :param cfg: DictConfig configuration composed by Hydra.
@@ -125,12 +125,21 @@ def main(cfg: DictConfig) -> Optional[float]:
         return -1
 
     # safely retrieve metric value for hydra-based hyperparameter optimization
-    metric_value = get_metric_value(
-        metric_dict=metric_dict, metric_name=cfg.get("optimized_metric")
-    )
+    if isinstance(cfg.get("optimized_metric"), str):
+        metric_value = get_metric_value(
+            metric_dict=metric_dict, metric_name=cfg.get("optimized_metric")
+        )
 
-    # return optimized metric
-    return metric_value
+        return metric_value
+    else:
+
+        metric_value = [
+            get_metric_value(
+                metric_dict=metric_dict, metric_name=metric_name
+            ) for metric_name in cfg.get("optimized_metric")
+        ]
+
+        return tuple(metric_value)
 
 
 if __name__ == "__main__":
